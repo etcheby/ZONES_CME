f6cf022e59d839a7b2cda07d7e21465c966edbe3
Eugene Tcheby <68718382+etcheby@users.noreply.github.com>
2021-03-26 14:30:28 -0400

    Update zone.sh

--- zone.sh
+++ zone.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 : '
-------- No supported in production -------
-Needs to be run in Autoprovision template with "ZONE" as a custom parameter and also 
+------- Not supported in production -------
+Needs to be run in Autoprovision template with "ZONE" as a custom parameter. 
 Rulebase name to install as second Parameter and Name of the GW where we will take the identities
 ------- No supported in production -------
 '
